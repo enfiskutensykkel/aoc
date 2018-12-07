a3c17753361b8bcb06281d2b4f8db99ec54bf8c9
Jonas Markussen <jonassm@ifi.uio.no>
2018-12-07 18:30:58 +0100

    cleanup

--- 2018/07/two.py
+++ 2018/07/two.py
@@ -3,10 +3,12 @@
 from collections import deque
 import re
 
-n_cost = 61
+n_cost = 60
 n_workers = 5
-
 instructions = open('input').read()
+
+#n_cost = 0
+#n_workers = 2
 #instructions = """Step C must be finished before step A can begin.
 #Step C must be finished before step F can begin.
 #Step A must be finished before step B can begin.
@@ -27,7 +29,7 @@ for step, depending in re.findall(r"Step ([A-Z]) must be finished before step ([
 
 
 
-queue = deque([(step, n_cost + (ord(step) - ord('A'))) for step in filter(lambda x: not x in deps, sorted(steps))])
+queue = deque([(step, n_cost + 1 + (ord(step) - ord('A'))) for step in filter(lambda x: not x in deps, sorted(steps))])
 
 string = ""
 workers = n_workers
@@ -51,14 +53,14 @@ while len(queue) > 0 or len(progress) > 0:
                 if step in deps[dep]:
                     deps[dep].remove(step)
                     if len(deps[dep]) == 0:
-                        queue.append((dep, n_cost + (ord(dep) - ord('A'))))
+                        queue.append((dep, n_cost + 1 + (ord(dep) - ord('A'))))
 
     while workers > 0 and len(queue) > 0:
         step, cost = queue.popleft()
         progress.append({'step': step, 'cost': cost})
         workers -= 1
 
-    print seconds, [p for p in progress], string
+    print seconds, [p['step'] for p in progress], string
     seconds += 1
 
-print seconds, string
+print seconds - 1, string
