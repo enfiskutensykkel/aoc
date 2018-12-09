9492150f1725aebb45d11686d9392b7a4641d6f8
Jonas Markussen <jonassm@ifi.uio.no>
2018-12-09 15:51:30 +0100

    a

--- 2018/09/9.c
+++ 2018/09/9.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <errno.h>
-
 
 struct marble
 {
@@ -15,30 +13,18 @@ struct marble
 int insert_marble(struct marble** current, uint32_t value)
 {
     struct marble* marble = malloc(sizeof(struct marble));
-    if (marble == NULL) 
-    {
-        return ENOMEM;
+    if (marble == NULL) {
+        return 1;
     }
 
-    marble->next = marble;
-    marble->prev = marble;
     marble->value = value;
-
-    if (*current == NULL)
-    {
-        *current = marble;
-    }
-
-
     marble->next = (*current)->next->next;
     marble->prev = (*current)->next;
     (*current)->next->next->prev = marble;
     (*current)->next->next = marble;
     *current = marble;
-
     return 0;
 }
-
 
 uint32_t remove_marble(struct marble** current)
 {
@@ -46,12 +32,10 @@ uint32_t remove_marble(struct marble** current)
 
     for (int i = 0; i < 7 && ptr != NULL; ++i, ptr = ptr->prev);
 
-    if (ptr != NULL)
-    {
+    if (ptr != NULL) {
         uint32_t value = ptr->value;
         *current = ptr->next;
-        if (ptr == *current)
-        {
+        if (ptr == *current) {
             *current = NULL;
         }
         ptr->next->prev = ptr->prev;
@@ -63,53 +47,29 @@ uint32_t remove_marble(struct marble** current)
     return 0;
 }
 
-
-void free_all(struct marble** current)
-{
-    struct marble* ptr = *current;
-    if (*current != NULL)
-    {
-       ptr = (*current)->next;
-    }
-
-    while (ptr != *current)
-    {
-        struct marble* next = ptr->next;
-        free(ptr);
-        ptr = next;
-    }
-
-    free(*current);
-    *current = NULL;
-}
-
-
 uint64_t play_game(int players, uint32_t last_marble)
 {
     uint64_t* scores = calloc(players, sizeof(uint64_t));
-    if (scores == NULL) 
-    {
+    if (scores == NULL) {
         return 0;
     }
 
-    struct marble* current = NULL;
-    if (insert_marble(&current, 0))
-    {
+    struct marble* current = malloc(sizeof(struct marble));
+    if (current == NULL) {
         free(scores);
         return 0;
     }
+    current->value = 0;
+    current->next = current;
+    current->prev = current;
 
     int player = 0;
     uint32_t next_marble = 1;
-    while (current->value != last_marble)
-    {
-        if (next_marble % 23 == 0)
-        {
+    while (current->value != last_marble) {
+        if (next_marble % 23 == 0) {
             scores[player] += next_marble;
             scores[player] += remove_marble(&current);
-        }
-        else
-        {
+        } else {
             insert_marble(&current, next_marble);
         }
 
@@ -117,14 +77,16 @@ uint64_t play_game(int players, uint32_t last_marble)
         player = (player + 1) % players;
     }
 
-    free_all(&current);
+    current->prev->next = NULL;
+    while (current != NULL) {
+        struct marble* next = current->next;
+        free(current);
+        current = next;
+    }
 
-    // Find maximum score
     uint64_t max = 0;
-    for (int i = 0; i < players; ++i) 
-    {
-        if (scores[i] > max) 
-        {
+    for (int i = 0; i < players; ++i) {
+        if (scores[i] > max) {
             max = scores[i];
         }
     }
@@ -133,13 +95,10 @@ uint64_t play_game(int players, uint32_t last_marble)
     return max;
 }
 
-
 int main()
 {
     uint64_t part1 = play_game(438, 71626);
     uint64_t part2 = play_game(438, 71626 * 100);
-
-    printf("%llu %llu\n", part1, part2);
-
+    printf("%llu\n%llu\n", part1, part2);
     return 0;
 }
