a8a177bc0e21dd436c77d40cb4bb5850e97e674c
Jonas Markussen <jonassm@ifi.uio.no>
2018-12-10 11:22:44 +0100

    cleanup

--- 2018/10/10.py
+++ 2018/10/10.py
@@ -2,6 +2,38 @@
 from __future__ import print_function
 import re
 
+#data = """position=< 9,  1> velocity=< 0,  2>
+#position=< 7,  0> velocity=<-1,  0>
+#position=< 3, -2> velocity=<-1,  1>
+#position=< 6, 10> velocity=<-2, -1>
+#position=< 2, -4> velocity=< 2,  2>
+#position=<-6, 10> velocity=< 2, -2>
+#position=< 1,  8> velocity=< 1, -1>
+#position=< 1,  7> velocity=< 1,  0>
+#position=<-3, 11> velocity=< 1, -2>
+#position=< 7,  6> velocity=<-1, -1>
+#position=<-2,  3> velocity=< 1,  0>
+#position=<-4,  3> velocity=< 2,  0>
+#position=<10, -3> velocity=<-1,  1>
+#position=< 5, 11> velocity=< 1, -2>
+#position=< 4,  7> velocity=< 0, -1>
+#position=< 8, -2> velocity=< 0,  1>
+#position=<15,  0> velocity=<-2,  0>
+#position=< 1,  6> velocity=< 1,  0>
+#position=< 8,  9> velocity=< 0, -1>
+#position=< 3,  3> velocity=<-1,  1>
+#position=< 0,  5> velocity=< 0, -1>
+#position=<-2,  2> velocity=< 2,  0>
+#position=< 5, -2> velocity=< 1,  2>
+#position=< 1,  4> velocity=< 2,  1>
+#position=<-2,  7> velocity=< 2, -2>
+#position=< 3,  6> velocity=<-1, -1>
+#position=< 5,  0> velocity=< 1,  0>
+#position=<-6,  0> velocity=< 2,  0>
+#position=< 5,  9> velocity=< 1, -2>
+#position=<14,  7> velocity=<-2,  0>
+#position=<-3,  6> velocity=< 2, -1>"""
+
 class Point(object):
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -13,9 +45,6 @@ class Point(object):
         self.x += self.vx
         self.y += self.vy
 
-    def coords(self):
-        return (self.x, self.y)
-
 
 def print_grid(grid):
     coords = [None] * 4
@@ -24,7 +53,6 @@ def print_grid(grid):
             coords[0] = x
         if coords[1] is None or x > coords[1]:
             coords[1] = x
-
         if coords[2] is None or y < coords[2]:
             coords[2] = y
         if coords[3] is None or y > coords[3]:
@@ -39,47 +67,13 @@ def print_grid(grid):
         print()
 
 
-data = """position=< 9,  1> velocity=< 0,  2>
-position=< 7,  0> velocity=<-1,  0>
-position=< 3, -2> velocity=<-1,  1>
-position=< 6, 10> velocity=<-2, -1>
-position=< 2, -4> velocity=< 2,  2>
-position=<-6, 10> velocity=< 2, -2>
-position=< 1,  8> velocity=< 1, -1>
-position=< 1,  7> velocity=< 1,  0>
-position=<-3, 11> velocity=< 1, -2>
-position=< 7,  6> velocity=<-1, -1>
-position=<-2,  3> velocity=< 1,  0>
-position=<-4,  3> velocity=< 2,  0>
-position=<10, -3> velocity=<-1,  1>
-position=< 5, 11> velocity=< 1, -2>
-position=< 4,  7> velocity=< 0, -1>
-position=< 8, -2> velocity=< 0,  1>
-position=<15,  0> velocity=<-2,  0>
-position=< 1,  6> velocity=< 1,  0>
-position=< 8,  9> velocity=< 0, -1>
-position=< 3,  3> velocity=<-1,  1>
-position=< 0,  5> velocity=< 0, -1>
-position=<-2,  2> velocity=< 2,  0>
-position=< 5, -2> velocity=< 1,  2>
-position=< 1,  4> velocity=< 2,  1>
-position=<-2,  7> velocity=< 2, -2>
-position=< 3,  6> velocity=<-1, -1>
-position=< 5,  0> velocity=< 1,  0>
-position=<-6,  0> velocity=< 2,  0>
-position=< 5,  9> velocity=< 1, -2>
-position=<14,  7> velocity=<-2,  0>
-position=<-3,  6> velocity=< 2, -1>"""
 data = open('input').read()
-
 points = []
 for p, v in re.findall(r'position=<([^>]*)> velocity=<([^>]*)>', data):
     x, y = [int(m) for m in re.findall(r'-?\d+', p)]
     vx, vy = [int(m) for m in re.findall(r'-?\d+', v)]
 
     points.append(Point(x, y, vx, vy))
-
-
 
 secs = 0
 prev_grid = None
@@ -89,15 +83,15 @@ while True:
 
     bottom = 10**6
     top = -10**6
-    for point in points:
-        if point.y < bottom:
-            bottom = point.y
+    for p in points:
+        if p.y < bottom:
+            bottom = p.y
 
-        if point.y > top:
-            top = point.y
+        if p.y > top:
+            top = p.y
 
-        grid.add(point.coords())
-        point.update()
+        grid.add((p.x, p.y))
+        p.update()
 
     height = top - bottom
 
@@ -107,7 +101,6 @@ while True:
         secs += 1
     else:
         break
-
 
 print_grid(prev_grid)
 print(secs - 1)
