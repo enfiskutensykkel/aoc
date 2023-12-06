635b7379b4a2371cbfb984b8d5fff6796fb0dce6
Jonas Markussen <jonas@dolphinics.com>
2023-12-06 14:37:14 +0100

    bug fix and refactoring

--- 2023/06/both.py
+++ 2023/06/both.py
@@ -2,8 +2,6 @@
 import re
 import sys
 
-# Part 1
-
 # x-axis = hold time
 # y-axis = race length-1
 #
@@ -15,36 +13,37 @@ import sys
 # 0 5 8 9 8 5 0
 #0 6 a c c a 6 0
 
+def ways(time, distance):
+    h = time // 2
+    l = 1
+    while l < h:
+        i = (l + h) // 2
+        d = i * (time - i)
+
+        if d > distance:
+            h = i - 1
+        elif d < distance:
+            l = i + 1
+        else:
+            break
+
+    d = h * (time - h)
+    while d > distance:
+        h -= 1
+        d = h * (time - h)
+
+    return (time - h*2 - 1)
+
 times, distances = ([int(x) for x in re.findall(r"\d+", line)]
                     for line in sys.stdin.read().strip().split("\n"))
 
 p = 1
 for t, d in zip(times, distances):
-    ways = 0
-    for i in range(1, t):
-        distance = i * (t - i)
-        if distance > d:
-            ways += 1
-    p *= ways
+    w = ways(t, d)
+    p *= w
 
 print(p)
 
-
-# Part 2
 time = int("".join(str(x) for x in times))
 distance = int("".join(str(x) for x in distances))
-
-h = time // 2
-l = 1
-while l < h:
-    i = (l + h) // 2
-    d = i * (time - i)
-
-    if d > distance:
-        h = i - 1
-    elif d < distance:
-        l = i + 1
-    else:
-        break
-
-print(time - h*2 -1)
+print(ways(time, distance))
