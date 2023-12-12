3cdaf61775e60c64d47fe2d4fcce6a4d309a6f87
Jonas Markussen <jonas@dolphinics.com>
2023-12-12 15:49:37 +0100

    made part 1 faster

--- 2023/12/1.py
+++ 2023/12/1.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from sys import stdin
 
-def seek(group):
+
+def test_arrangement(group, counts):
     i = 0
     j = -1
     streaks = []
     while i < len(group):
-        if group[i] != '#':
+        if group[i] == '.':
             if j != i - 1:
                 streaks.append(i - j - 1)
             j = i
@@ -15,28 +16,35 @@ def seek(group):
     if j != i - 1:
         streaks.append(i - j - 1)
 
-    return streaks
+    return streaks == counts
 
-def permutations(group, counts, start):
-    if start >= len(group):
-        return 1 if seek(group) == counts else 0
+
+def permutate(group, counts, start, dmg, op):
+    if start >= len(group) or (dmg == 0 and op == 0):
+        return 1 if test_arrangement(group, counts) else 0
 
     p = 0
     if group[start] == '?':
-        group[start] = '#'
-        p += permutations(group, counts, start + 1)
-        group[start] = '.'
-        p += permutations(group, counts, start + 1)
+        if dmg > 0:
+            group[start] = '#'
+            p += permutate(group, counts, start+1, dmg-1, op)
+
+        if op > 0:
+            group[start] = '.'
+            p += permutate(group, counts, start+1, dmg, op-1)
+
         group[start] = '?'
+
     else:
-        p = permutations(group, counts, start + 1)
+        p = permutate(group, counts, start + 1, dmg, op)
     return p
 
 
 s = 0
-for row in stdin.read().strip().split('\n'):
+for row in stdin.read().strip().split("\n"):
     group, counts = row.split()
-    l = permutations(list(group), [int(x) for x in counts.split(',')], 0)
+    counts = [int(x) for x in counts.split(",")]
+    l = permutate(list(group), counts, 0, sum(counts), len(group) - sum(counts))
     s += l
 
 print(s)
