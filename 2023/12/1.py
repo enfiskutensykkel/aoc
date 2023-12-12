94a52a141cc3eb95d49e9ab595b96dd77939af51
Jonas Markussen <jonas@dolphinics.com>
2023-12-12 16:27:38 +0100

    faster part 1

--- 2023/12/1.py
+++ 2023/12/1.py
@@ -5,45 +5,56 @@ from sys import stdin
 def test_arrangement(group, counts):
     i = 0
     j = -1
-    streaks = []
+
+    streaks = 0
+
     while i < len(group):
         if group[i] == '.':
             if j != i - 1:
-                streaks.append(i - j - 1)
+                if streaks >= len(counts) or counts[streaks] != i - j - 1:
+                    return False
+                streaks += 1
             j = i
         i += 1
 
     if j != i - 1:
-        streaks.append(i - j - 1)
+        if streaks >= len(counts) or counts[streaks] != i - j - 1:
+            return False
+        streaks += 1
 
-    return streaks == counts
+    return streaks == len(counts)
 
 
 def permutate(group, counts, start, dmg, op):
     if start >= len(group) or (dmg == 0 and op == 0):
         return 1 if test_arrangement(group, counts) else 0
 
-    p = 0
     if group[start] == '?':
+        permutations = 0
+
         if dmg > 0:
             group[start] = '#'
-            p += permutate(group, counts, start+1, dmg-1, op)
+            p = permutate(group, counts, start+1, dmg-1, op)
+            permutations += p
 
         if op > 0:
             group[start] = '.'
-            p += permutate(group, counts, start+1, dmg, op-1)
+            p = permutate(group, counts, start+1, dmg, op-1)
+            permutations += p
 
         group[start] = '?'
 
     else:
-        p = permutate(group, counts, start + 1, dmg, op)
-    return p
+        permutations = permutate(group, counts, start + 1, dmg, op)
+
+    return permutations
 
 
 s = 0
 for row in stdin.read().strip().split("\n"):
     group, counts = row.split()
     counts = [int(x) for x in counts.split(",")]
+
     l = permutate(list(group), counts, 0, sum(counts), len(group) - sum(counts))
     s += l
 
