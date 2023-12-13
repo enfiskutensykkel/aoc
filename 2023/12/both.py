b557feea45502a320ba5c4e3b6233a7557cfe79e
Jonas Markussen <jonas@dolphinics.com>
2023-12-13 10:04:42 +0100

    Day 12 part 2

---
+++ 2023/12/both.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+from sys import stdin
+
+
+def test_arrangement(group, start, count):
+    i = start
+    length = start + count
+
+    while i < length:
+        if i >= len(group):
+            return False
+
+        if group[i] == ".":
+            return False
+
+        i += 1
+
+    if i < len(group) and group[i] == "#":
+        return False
+
+    return True
+
+
+def permutations(group, counts, start, idx, matches):
+    if idx >= len(counts):
+        while start < len(group):
+            if group[start] == "#":
+                return 0
+            start += 1
+        return 1
+
+    key = (group[start:], counts[idx:])
+    if key in matches:
+        return matches[key]
+
+    while start < len(group) and group[start] == ".":
+        start += 1
+
+    if start >= len(group):
+        return 1 if idx >= len(counts) else 0
+
+    count = counts[idx]
+
+    p = 0
+    if test_arrangement(group, start, count):
+        p += permutations(group, counts, start+count+1, idx + 1, matches)
+
+    if group[start] != "#":
+        p += permutations(group, counts, start+1, idx, matches)
+
+    matches[key] = p
+    return p
+
+
+matches = {}
+total_part1 = 0
+total_part2 = 0
+for row in stdin.read().strip().split("\n"):
+    group, counts = row.split()
+
+    long_group = "?".join([group] * 5)
+    long_counts = tuple(int(x) for x in ",".join([counts] * 5).split(","))
+
+    counts = tuple(int(x) for x in counts.split(","))
+
+    total_part1 += permutations(group, counts, 0, 0, matches)
+    total_part2 += permutations(long_group, long_counts, 0, 0, matches)
+
+print(total_part1)
+print(total_part2)
