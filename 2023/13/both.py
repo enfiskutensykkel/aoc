bb127e5568e90cce9cda563d940ccdf2a293fad0
Jonas Markussen <jonas@dolphinics.com>
2023-12-13 17:59:51 +0100

    refactoring

--- 2023/13/both.py
+++ 2023/13/both.py
@@ -46,10 +46,10 @@ for pattern in stdin.read().strip().split("\n\n"):
     width = pattern.index("\n")
 
     grid = [c for line in pattern.splitlines() for c in line]
+    rotated = None
     height = len(grid) // width
 
     vline = reflection(width, height, grid, False)
-
     if vline is None:
         rotated = rotate(width, height, grid)
         hline = reflection(height, width, rotated, False)
@@ -58,14 +58,13 @@ for pattern in stdin.read().strip().split("\n\n"):
         part1_total += (vline + 1)
 
     vline2 = reflection(width, height, grid, True)
-
     if vline2 is None:
-        rotated = rotate(width, height, grid)
+        if rotated is None:
+            rotated = rotate(width, height, grid)
         hline2 = reflection(height, width, rotated, True)
         part2_total += (hline2 + 1) * 100
     else:
         part2_total += (vline2 + 1)
-
 
 print(part1_total)
 print(part2_total)
