a86148c472e325feb9648414d42950005ee20974
Jonas Markussen <jonas@dolphinics.com>
2023-12-14 15:51:44 +0100

    Day 14 part 1

---
+++ 2023/14/1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from sys import stdin
+
+
+rows = []
+
+loads = []
+
+for y, row in enumerate(stdin.read().strip().splitlines()):
+
+    loads.insert(0, 0)
+    rows.insert(0, set())
+
+    for x, c in enumerate(row):
+        if c == '#':
+            rows[0].add(x)
+
+        elif c == 'O':
+            to_row = 0
+            while to_row+1 < len(rows) and x not in rows[to_row+1]:
+                to_row += 1
+
+            rows[to_row].add(x)
+            loads[to_row] += 1
+
+print(sum(l * (i+1) for i, l in enumerate(loads)))
